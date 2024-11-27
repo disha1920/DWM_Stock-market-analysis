3bfbdccadb663c23fc0b1cdbb2060c8e9fcca0d5
Disha <123107294+disha1920@users.noreply.github.com>
2024-11-27 17:25:27 +0530

    Update ws.py

--- ws.py
+++ ws.py
@@ -1,4 +1,5 @@
 import os
 from app import app
 if_name="_main_":
-port = int(os.environ.get("PORT", 4000)) app.run(host="0.0.ele", port=port)|
+port = int(os.environ.get("PORT", 4000)) 
+app.run(host="0.0.0.0", port=port)|
